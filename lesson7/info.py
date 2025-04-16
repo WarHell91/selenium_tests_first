af0c85c945aa49d3ed467d1ed7f61401092065bb
DESKTOP-MP28EB8\WarHell <WarHell91@mail.ru>
2025-04-16 19:45:44 +0400

    123

---
+++ lesson7/info.py
@@ -0,0 +1,9 @@
+
+－Атрибуты и конструкции из видео:
+➖ Глобальный поиск - //header
+➖ Вложенность - //header/div
+➖ Порядковый номер - (//header/div)[2]
+➖ Поиск по атрибуту - //input[@type='submit']
+➖ Поиск по параметру text - //button[text()='Sign in']
+➖ Поиск по содержимому - //button[contains(@class, 'btn')]
+➖ Поиск по нескольким атрибутам - //input[@type='email' and @role='login']
